ec1f3123d3757e9c1e81b54021abc80895cf8451
Yuki xD <pxnditxyr@gmail.com>
2023-01-04 19:01:17 -0400

    feat: added connection to database

--- actions/editar_refinanciamiento.php
+++ actions/editar_refinanciamiento.php
@@ -1,8 +1,26 @@
 <?php
  // get post method refinanciamiento value
-  if ( !isset( $_POST[ 'refinanciamiento' ] ) ) {
+  if ( !isset( $_POST[ 'refinanciamiento' ] ) || !isset( $_POST[ 'id' ] ) ) {
     header( 'Location: ../index.php' );
   } 
+
   $refinanciamiento = $_POST[ 'refinanciamiento' ];
-  echo $refinanciamiento;
+
+  $host = 'localhost';
+  $user = 'root';
+  $clave = '';
+  $bd = 'xd';
+  $conectar = mysqli_connect($host,$user,$clave,$bd);
+
+  // $consulta = 'select * from prestamos';
+  // $Rconsulta =  'select * from prestamos';
+  // $Resultado = mysqli_query($conectar,$consulta);
+  // sql for update
+  $sql = "UPDATE prestamos SET refinanciamiento = '$refinanciamiento' WHERE id = '$_POST[id]'";
+  // execute query
+  if ( mysqli_query( $conectar, $sql ) ) {
+    header( 'Location: ../index.php' );
+  } else {
+    echo "Error updating record: " . mysqli_error( $conectar );
+  }
 ?>
